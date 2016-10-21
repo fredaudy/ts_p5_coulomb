dec1d14941b283400a32a92ec6bc12fd6775a618
fredaudy <fredaudy@gmail.com>
2016-10-22 01:50:59 +0200

    petits changements 1

--- tsFiles/sketch.ts
+++ tsFiles/sketch.ts
@@ -5,10 +5,12 @@ let pt: Coulomb[] = [];
 
 function setup() {
     prg.createCanvas(1200, 700);
-    prg.background(125);
+    prg.background(175);
 
     for (let i = 0; i < 15; i++) {
         pt.push(new Coulomb(prg.width * (i + 1) / 16, prg.height * (i + 1) / 16, 1));
+        prg.fill(175);
+        prg.stroke(255);
         prg.ellipse(pt[i].point.x, pt[i].point.y, 20, 20);
     }
 }
